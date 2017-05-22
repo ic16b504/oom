10f8b737aad052dd16343f5f1b224b5d2b1a6b51
ic16b504 <florian.stefan.schmidt@gmail.com>
2017-05-22 23:08:43 +0200

    add project Task4

---
+++ tasks/Task4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var items = new Items[]
+            {
+                    new Auto("Aston Martin", "", 400, Currency.EUR),
+                    new Auto("Ford", "", 200, Currency.EUR),
+                    new Auto("VW", "", 300, Currency.EUR),
+            };
+
+            var currency = Currency.EUR;
+            foreach (var i in items)
+            {
+                Console.WriteLine($"{i.Description} {i.GetPrice(currency)} {currency}");
+            }
+            Serialization.Run(items);
+        }
+    }
+}
---
+++ tasks/Task4/Task4/Serialization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Task4
+{
+    class Serialization
+    {
+
+        public static void Run(Items[] itmes)
+        {
+            var auto = itmes[0];
+
+            File.WriteAllText(@"c:\itmes.json", JsonConvert.SerializeObject(auto));
+
+            using (StreamWriter file = File.CreateText(@"c:\itmes.json"))
+            {
+                JsonSerializer serial = new JsonSerializer();
+                serial.Serialize(file, auto);
+            }
+        }
+    }
+}
